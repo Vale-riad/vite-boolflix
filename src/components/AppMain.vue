f5b45351bb7890e5cfb86482e7b002efb9c5d0ae
Vale-riad <valentinariad91@gmail.com>
2022-11-30 12:00:46 +0100

    Modifica struttura di base

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,22 +1,12 @@
 <script>
 import { store } from "../store";
-import AppCard from "./AppCard.vue";
 export default {
   name: "AppMain",
-  components: {
-    AppCard,
-  },
-  data() {
-    return {
-      store,
-    };
-  },
 };
 </script>
 
 <template>
   <h1>Main</h1>
-  <AppCard />
 </template>
 
 <style lang="scss" scoped></style>
